50f242ccd692dd483288e228630e82f044148a3c
Petr Smolik <petr.smolik.97@email.cz>
2025-01-12 02:36:33 +0100

    Detail product page enhanced

--- app/ui/main/ProductGallery.tsx
+++ app/ui/main/ProductGallery.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import { MainImageSkeleton } from '@/app/ui/skeletons';
 
@@ -31,6 +31,19 @@ export default function ProductGallery({ images, productName }: { images: string
     }
     setShowSkeleton(false); // Hide skeleton immediately when image loads
   };
+
+  useEffect(() => {
+    // Show skeleton with delay on initial load
+    skeletonTimeoutRef.current = setTimeout(() => {
+      setShowSkeleton(true);
+    }, 200); // Delay duration (200ms)
+
+    return () => {
+      if (skeletonTimeoutRef.current) {
+        clearTimeout(skeletonTimeoutRef.current); // Clear timeout on unmount
+      }
+    };
+  }, []);
 
   return (
     <>
@@ -75,21 +88,39 @@ function MainProductImage({ mainImage, productName, showSkeleton, onLoadComplete
   );
 }
 
-function MiniProductGallery({ images, productName, mainImage, onMainImageChange }: 
+export function MiniProductGallery({ images, productName, mainImage, onMainImageChange }: 
   { images: string[], productName: string, mainImage: string, onMainImageChange: (image: string) => void }) {
+  
+  const [loadingStates, setLoadingStates] = useState<boolean[]>(images.map(() => true)); // Initial loading states
+
+  const handleImageLoadComplete = (index: number) => {
+    setLoadingStates((prevStates) => {
+      const newStates = [...prevStates];
+      newStates[index] = false; // Mark image as loaded
+      return newStates;
+    });
+  };
+
   return (
     <div className="flex mt-3 space-x-2">
       {images.map((image, index) => (
-        <Image
-          key={index}
-          src={`/api/image?src=${encodeURIComponent(image)}&height=${90}`}
-          alt={`Image of ${productName} ${index + 1}`}
-          className={`w-[90px] h-[90px] rounded-lg object-contain cursor-pointer 
-            ${mainImage === image ? 'ring-2 ring-primary' : ''}`}
-          width={90}
-          height={65}
-          onClick={() => onMainImageChange(image)} // Update selected image on click
-        />
+        <div key={index} className="relative w-[90px] h-[90px]">
+          {/* Skeleton for small image */}
+          {loadingStates[index] && (
+            <div className="absolute inset-0 bg-gray-300 animate-pulse rounded-lg" />
+          )}
+
+          <Image
+            src={`/api/image?src=${encodeURIComponent(image)}&height=${90}`}
+            alt={`Image of ${productName} ${index + 1}`}
+            className={`w-full h-full rounded-lg object-contain cursor-pointer 
+              ${mainImage === image ? 'ring-2 ring-primary' : ''}`}
+            width={90}
+            height={65}
+            onLoadingComplete={() => handleImageLoadComplete(index)} // Trigger on load completion
+            onClick={() => onMainImageChange(image)}
+          />
+        </div>
       ))}
     </div>
   );
